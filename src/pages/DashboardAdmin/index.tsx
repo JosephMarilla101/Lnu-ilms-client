d3307debf676ff51d1b64c291fa77d4fe1ab36cc
JD-Rosales <rosales.jake1999@gmail.com>
2023-09-11 17:48:31 +0800

    Done admin dashboard

--- src/pages/DashboardAdmin/index.tsx
+++ src/pages/DashboardAdmin/index.tsx
@@ -9,21 +9,45 @@ import {
   Users2,
   LucideIcon,
 } from 'lucide-react';
+import {
+  useTotalBooks,
+  useTotalUnreturnedBooks,
+  useTotalRequestedBooks,
+  useTotalAuthors,
+  useTotalCatoegories,
+  useTotalStudents,
+  useTotalLibrarians,
+} from '@/hooks/useDashboard';
+import { useNavigate } from 'react-router-dom';
 
 const DashboardAdmin = () => {
+  const totalBooks = useTotalBooks();
+  const totalUnreturnedBooks = useTotalUnreturnedBooks();
+  const totalRequestedBooks = useTotalRequestedBooks();
+  const totalAuthors = useTotalAuthors();
+  const totalCatoegories = useTotalCatoegories();
+  const totalStudents = useTotalStudents();
+  const totalLibrarians = useTotalLibrarians();
+  const navigate = useNavigate();
+
   const Card = ({
     icon: Icon,
     title,
     count,
     color,
+    url,
   }: {
     icon: LucideIcon;
     title: string;
-    count: number;
+    count?: number;
     color: string;
+    url: string;
   }) => {
     return (
-      <div className='w-[180px] h-[200px] px-3 text-center bg-[#FFFFF7] rounded-xl overflow-hidden shadow-lg hover:shadow-xl cursor-pointer flex flex-col  hover:scale-105 transition duration-300 ease-in-out'>
+      <div
+        onClick={() => navigate(`${url}`)}
+        className='w-[180px] h-[200px] px-3 text-center bg-[#FFFFF7] rounded-xl overflow-hidden shadow-lg hover:shadow-xl cursor-pointer flex flex-col  hover:scale-105 transition duration-300 ease-in-out'
+      >
         <span
           className={`text-base font-semibold text-center pt-3 pb-2 ${color}`}
         >
@@ -45,44 +69,56 @@ const DashboardAdmin = () => {
         icon={Book}
         color='text-green-700'
         title='Books Listed'
-        count={20}
+        count={totalBooks.data}
+        url='/books'
       />
 
       <Card
         icon={History}
         color='text-red-700'
         title='Unreturned Books'
-        count={20}
+        count={totalUnreturnedBooks.data}
+        url='/book/issued'
       />
 
       <Card
         icon={BookUp}
         color='text-blue-600'
         title='Book Requests'
-        count={20}
+        count={totalRequestedBooks.data}
+        url='/book/requests'
       />
 
       <Card
         icon={UserSquare}
         color='text-yellow-700'
         title='Authors Listed'
-        count={20}
+        count={totalAuthors.data}
+        url='/authors'
       />
 
       <Card
         icon={ScrollText}
         color='text-emerald-600'
         title='Categories Listed'
-        count={20}
+        count={totalCatoegories.data}
+        url='/categories'
       />
 
-      <Card icon={Users} color='text-primary' title='Students' count={20} />
+      <Card
+        icon={Users}
+        color='text-primary'
+        title='Students'
+        count={totalStudents.data}
+        url='/students'
+      />
 
       <Card
         icon={Users2}
         color='text-secondary'
         title='Librarians'
-        count={20}
+        count={totalLibrarians.data}
+        url='/librarians'
       />
     </div>
   );
