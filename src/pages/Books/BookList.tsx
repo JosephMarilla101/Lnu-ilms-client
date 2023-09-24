294d96b84c556a4f1f1d5b3347f44a5201d153d6
JD-Rosales <rosales.jake1999@gmail.com>
2023-09-24 12:49:07 +0800

    123

--- src/pages/Books/BookList.tsx
+++ src/pages/Books/BookList.tsx
@@ -1,9 +1,19 @@
 import { useBookList } from '@/hooks/useBook';
+import { useGetActiveCategories } from '@/hooks/useCategory';
 import BookSkeletonLoader from '@/components/Skeletons/BookSkeletonLoader';
 import { Suspense, lazy, useEffect, useState } from 'react';
 import { cn } from '@/lib/utils';
 import { Input } from '@/components/ui/input';
 import { useInView } from 'react-intersection-observer';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
 const BookCard = lazy(() => import('@/components/BookCard'));
 
@@ -13,7 +23,9 @@ type BookListProps = {
 
 const BookList: React.FC<BookListProps> = ({ className }) => {
   const [filter, setFilter] = useState('');
-  const bookList = useBookList(filter);
+  const [categoryFiler, setCategoryFilter] = useState('');
+  const bookList = useBookList(filter, categoryFiler);
+  const activeCategories = useGetActiveCategories();
 
   const { ref, inView } = useInView({
     threshold: 0.5,
@@ -29,18 +41,45 @@ const BookList: React.FC<BookListProps> = ({ className }) => {
   useEffect(() => {
     bookList.refetch();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filter]);
+  }, [filter, categoryFiler]);
 
   return (
     <div className={cn('mt-4', className)}>
-      <Input
-        value={filter}
-        onChange={(e) => {
-          setFilter(e.target.value);
-        }}
-        placeholder='Search Books'
-        className='mb-6 max-w-xs'
-      />
+      <div className='flex flex-row gap-4'>
+        <Input
+          value={filter}
+          onChange={(e) => {
+            setFilter(e.target.value);
+          }}
+          placeholder='Search Books'
+          className='mb-6 max-w-xs'
+        />
+
+        <Select
+          value={categoryFiler}
+          onValueChange={(value) => {
+            setCategoryFilter(value);
+          }}
+        >
+          <SelectTrigger className='w-[180px]'>
+            <SelectValue placeholder='Filter by Category' />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Categories</SelectLabel>
+              <SelectItem value=''>None</SelectItem>
+              {activeCategories.data &&
+                activeCategories.data.map((category) => {
+                  return (
+                    <SelectItem value={category.name} key={category.id}>
+                      {category.name}
+                    </SelectItem>
+                  );
+                })}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
 
       {bookList.isLoading ? (
         <div className='flex flex-row gap-3 gap-x-6 flex-wrap justify-items-start'>
